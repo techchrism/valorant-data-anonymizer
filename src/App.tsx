01266aee8e7ba79cbd054811c558edcaadda6664
techchrism <chris@techchrism.me>
2023-05-13 14:37:08 -0700

    sync scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import type {Component} from 'solid-js'
-import {createMemo, createSignal} from 'solid-js'
+import {createEffect, createMemo, createSignal} from 'solid-js'
 import {anonymize} from './anonymize'
 
 const App: Component = () => {
     const [inputText, setInputText] = createSignal('')
+    const [scrollPosition, setScrollPosition] = createSignal(0)
+    let inputTextArea: HTMLTextAreaElement
+    let outputTextArea: HTMLTextAreaElement
 
     const outputText = createMemo(() => {
         return anonymize(inputText())
+    })
+
+    createEffect(() => {
+        const scrollPos = scrollPosition()
+        inputTextArea.scrollTop = scrollPos
+        outputTextArea.scrollTop = scrollPos
     })
 
     return (
@@ -17,6 +26,8 @@ const App: Component = () => {
                     <div class="flex-grow flex flex-col">
                         <label for="input" class="text-center text-xl mb-2">Input</label>
                         <textarea
+                            ref={inputTextArea}
+                            onScroll={(e) => setScrollPosition(e.currentTarget.scrollTop)}
                             id="input"
                             onInput={(e) => setInputText(e.target.value)}
                             class="flex-grow resize-none bg-slate-100 text-slate-900 dark:bg-slate-950 dark:text-slate-50 rounded-2xl p-2"
@@ -25,6 +36,8 @@ const App: Component = () => {
                     <div class="flex-grow flex flex-col">
                         <label for="output" class="text-center text-xl mb-2">Output</label>
                         <textarea
+                            ref={outputTextArea}
+                            onScroll={(e) => setScrollPosition(e.currentTarget.scrollTop)}
                             readonly
                             value={outputText()}
                             id="output"
